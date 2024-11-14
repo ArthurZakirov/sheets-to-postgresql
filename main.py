01008c997e039a5b929b8536c523c580a8398b3e
Arthur Zakirov <arthur.zak99@gmail.com>
2024-11-14 09:20:55 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from prefect.server.schemas.schedules import IntervalSchedule
+from prefect.client.schemas.schedules import IntervalSchedule
 from datetime import timedelta
 from prefect import flow
 import os
@@ -12,10 +12,10 @@ STAGING_DIR = os.getenv("STAGING_DIR")
 if __name__ == "__main__":
     pulled_flow = flow.from_source(
         source=SOURCE_REPO,
-        entrypoint="main.py:etl_flow"
+        entrypoint="src/flows/etl_flow.py:etl_flow"
     )
     pulled_flow.deploy(
-        name="my-first-deployment",
+        name="sheets-to-azure-elt",
         work_pool_name="default-work-pool",
-        schedule=IntervalSchedule(interval=timedelta(days=1))
+        schedules=[IntervalSchedule(interval=timedelta(days=1))]
     )
